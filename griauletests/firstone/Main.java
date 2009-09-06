a720a513d862fba97d26622d70ffdd210541e5bc
matheus.motta <matheus.motta@2ba458a8-7886-11de-b03f-d78e5a28ce11>
2009-09-06 07:10:20 +0000

    Final version off firstone example.
    -Connection with csji mysql DB.
    -License inside the project problem solved

--- griauletests/firstone/Main.java
+++ griauletests/firstone/Main.java
@@ -1,20 +1,19 @@
-
-
 package griauletests.firstone;
 
 /**
- *
+ * 
  * @author Matheus Motta
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-       Principal teste = new Principal();
-     
-    }
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) {
+
+		Principal teste = new Principal();
+
+	}
 
 }
