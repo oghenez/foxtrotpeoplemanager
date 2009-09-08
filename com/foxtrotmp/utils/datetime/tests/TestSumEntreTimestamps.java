35f3b3a0c016fef27ab975c0dc1b79e92a88bce3
matheus.motta <matheus.motta@gmail.com>
2009-09-08 08:31:53 +0000

    Starting the timestamp handling

---
+++ com/foxtrotmp/utils/datetime/tests/TestSumEntreTimestamps.java
@@ -0,0 +1,13 @@
+package com.foxtrotmp.utils.datetime.tests;
+
+public class TestSumEntreTimestamps {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
