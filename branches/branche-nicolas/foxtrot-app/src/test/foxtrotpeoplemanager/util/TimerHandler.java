1c1c276283a3048d5fb38df6b501f133fdf0108e
nk.oliveira <nk.oliveira@2ba458a8-7886-11de-b03f-d78e5a28ce11>
2009-09-09 05:37:36 +0000

    Teste com horas

--- branches/branche-nicolas/foxtrot-app/src/test/foxtrotpeoplemanager/util/TimerHandler.java
+++ branches/branche-nicolas/foxtrot-app/src/test/foxtrotpeoplemanager/util/TimerHandler.java
@@ -3,10 +3,47 @@
  */
 package test.foxtrotpeoplemanager.util;
 
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
- * @author nicolas
- *
- */
+ * Implementation of time handler functions.
+ * 
+ * @version: 1.0
+ * @author: Nicolas Oliveira
+ **/
 public class TimerHandler {
 
+	
+	/**
+	 * This method receives a timestamp as String and return a object
+	 * sql.Timestamp.
+	 * 
+	 * @param timestamp
+	 *            The timestamp as String (Probably from a mysql database)
+	 * @return sql.Timestamp based on timestampString
+	 * 
+	 * @throws ParseException
+	 *             on the conversion
+	 */
+	public Timestamp timeStringToTimestamp(String timestamp){
+		
+		SimpleDateFormat timeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		
+		Timestamp ts = null;
+		Date date;
+		
+		try {
+			date = timeFormat.parse(timestamp);
+			ts = new Timestamp(date.getTime());
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	
+		
+		return ts;
+			
+	}
 }
