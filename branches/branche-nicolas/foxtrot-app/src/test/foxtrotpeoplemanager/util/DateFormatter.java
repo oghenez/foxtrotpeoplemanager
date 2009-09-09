c71dae7cf5d47d7431bf7349eed0d76701f86335
nk.oliveira <nk.oliveira@2ba458a8-7886-11de-b03f-d78e5a28ce11>
2009-09-09 05:38:39 +0000

    Manipular data

--- branches/branche-nicolas/foxtrot-app/src/test/foxtrotpeoplemanager/util/DateFormatter.java
+++ branches/branche-nicolas/foxtrot-app/src/test/foxtrotpeoplemanager/util/DateFormatter.java
@@ -4,9 +4,13 @@
 package test.foxtrotpeoplemanager.util;
 
 /**
+ * DateFormatter. Funcionalidade de formatar datas de diversas formas
  * @author nicolas
  *
  */
 public class DateFormatter {
-
+	
+	private String format = "yyyy-MM-dd HH:mm:ss";
+	
+	
 }
