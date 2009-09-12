51cde33057983930b5b6188858c36a2112ad26a1
matheus.motta <matheus.motta@2ba458a8-7886-11de-b03f-d78e5a28ce11>
2009-09-12 23:06:48 +0000

    Modificações estruturais no script do banco

--- database/scripts/foxtrot_intial_database.sql
+++ database/scripts/foxtrot_intial_database.sql
@@ -12,20 +12,20 @@ create table people	(
 	constraint pkpeople	primary key(id, cpf)
 );
 
-
 create table wayinout	(
-			id	 			int not null auto_increment,
-			id_in			int not null auto_increment,
-			id_out			int null,
-			id_people 		int not null,
-			id_rec_mode		int not null,
-			id_location		int not null,
-			id_sensor		int not null,
-			id_wayout		int not null,
-			id_wayin		int not null,
-			db_timestamp	timestamp,
-			sys_timestamp	timestamp,
-
+			id	 				int not null auto_increment,
+			id_in				int not null auto_increment, 
+			id_out				int	not	null auto_increment,
+			id_people 			int not null,
+			id_rec_mode_in		int not null,
+			id_rec_mode_out		int not null,
+			id_sensor_in		int not null,
+			id_sensor_out		int not null,
+			db_timestamp_in		timestamp,
+			sys_timestamp_in	timestamp,
+			db_timestamp_out	timestamp,
+			sys_timestamp_out	timestamp,
+			
 	constraint  pkentrance primary key (id, id_in, id_out)
 );
 
@@ -33,6 +33,7 @@ create table wayinout	(
 create table sensor	(
 			id 				int not null auto_increment,
 			sensor_uuid		int not null,
+			id_location		int not null,
 			id_status		int not null,
 			id_rec_mode		int not null,
 			comment			varchar(500) not null,
@@ -63,6 +64,14 @@ create table recognition_mode	(
 			comment 		varchar(3000),
 	
 	constraint pkrec_mode primary key(id)
+);
+
+
+create table log	(
+			id 				int not null auto_increment,
+			comment 		varchar(3000),
+	
+	constraint pklog primary key(id)
 );
 
 --Constraints for wayin table:
