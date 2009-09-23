2dc9301a9c0326a4d13046db906da215d0d104fd
matheus.motta <matheus.motta@2ba458a8-7886-11de-b03f-d78e5a28ce11>
2009-09-23 03:56:01 +0000

    Corrigindo problemas de referência com o applet exemplo griaule

--- trunk/src/griauletests/applet/sample/com/griaule/fingerprintsdk/appletsample/AppletInstaller.java
+++ trunk/src/griauletests/applet/sample/com/griaule/fingerprintsdk/appletsample/AppletInstaller.java
@@ -30,8 +30,8 @@
  documentation and/or sample.
 
  -------------------------------------------------------------------------------
-*/
-package com.griaule.fingerprintsdk.appletsample;
+ */
+package griauletests.applet.sample.com.griaule.fingerprintsdk.appletsample;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -44,92 +44,89 @@ import java.util.zip.ZipInputStream;
 import com.griaule.grfingerjava.GrFingerJava;
 import com.griaule.grfingerjava.GrFingerJavaException;
 
-
-
 /**
  * This class is used to copy all native libraries used by Fingerprint SDK and
  * load them into the Java VM.
  */
 public class AppletInstaller {
 
-    /**Code for Microsoft Windows. */
-    private static final int WINDOWS_OS = 0;
-    /**Code for Linux */
-    private static final int LINUX_OS = 1;
-    /**Code for Unknown OS. */
-    private static final int UNKNOWN_OS = 2;
+	/** Code for Microsoft Windows. */
+	private static final int WINDOWS_OS = 0;
+	/** Code for Linux */
+	private static final int LINUX_OS = 1;
+	/** Code for Unknown OS. */
+	private static final int UNKNOWN_OS = 2;
 
-    /**Flag which indicates if Fingerprint SDK has already been installed.*/
-    private static boolean installed = false;
+	/** Flag which indicates if Fingerprint SDK has already been installed. */
+	private static boolean installed = false;
 
-    /**
-     * Copies Fingerprint SDK native libraries placed on the specified URL to a
-     * local folder and make them available to thr JVM.
-     */
-    public synchronized static void install(URL ZipFile)
-    throws IOException, UnsupportedOperationException, GrFingerJavaException {
+	/**
+	 * Copies Fingerprint SDK native libraries placed on the specified URL to a
+	 * local folder and make them available to thr JVM.
+	 */
+	public synchronized static void install(URL ZipFile) throws IOException,
+			UnsupportedOperationException, GrFingerJavaException {
 
-        //Skips re-installation.
-        if (installed)
-            return;
+		// Skips re-installation.
+		if (installed)
+			return;
 
-        //Create temporary folder as the destination of the native libraries/license file
-        File destDir = File.createTempFile("FingerprintSDKApplet", "tmp");
-        destDir.delete();
-        destDir.mkdirs();
-        destDir.deleteOnExit();
+		// Create temporary folder as the destination of the native
+		// libraries/license file
+		File destDir = File.createTempFile("FingerprintSDKApplet", "tmp");
+		destDir.delete();
+		destDir.mkdirs();
+		destDir.deleteOnExit();
 
-        //Extracts the contents of the zip file
-        ZipInputStream zipStream=new ZipInputStream(ZipFile.openStream());
-        ZipEntry zipEntry;
-        while ((zipEntry=zipStream.getNextEntry())!=null) {
-            File f = new File(destDir, zipEntry.getName());
-            if (zipEntry.isDirectory()) {
-                f.mkdirs();
-            } else {
-                OutputStream out = new FileOutputStream(f);
+		// Extracts the contents of the zip file
+		ZipInputStream zipStream = new ZipInputStream(ZipFile.openStream());
+		ZipEntry zipEntry;
+		while ((zipEntry = zipStream.getNextEntry()) != null) {
+			File f = new File(destDir, zipEntry.getName());
+			if (zipEntry.isDirectory()) {
+				f.mkdirs();
+			} else {
+				OutputStream out = new FileOutputStream(f);
 
-                byte[] buffer = new byte[4096];
-                while (true) {
-                    int bytesRead = zipStream.read(buffer);
-                    if (bytesRead == -1)
-                        break;
-                    out.write(buffer,0,bytesRead);
-                }
-                out.close();
-            }
-        }
+				byte[] buffer = new byte[4096];
+				while (true) {
+					int bytesRead = zipStream.read(buffer);
+					if (bytesRead == -1)
+						break;
+					out.write(buffer, 0, bytesRead);
+				}
+				out.close();
+			}
+		}
 
+		GrFingerJava.setNativeLibrariesDirectory(destDir);
+		GrFingerJava.setLicenseDirectory(destDir);
+		installed = true;
+	}
 
+	/**
+	 * Finds out which OS / Plataform this JVM is being runned on.
+	 */
+	private static int getOS() {
+		// Loads the OS name / CPu architecture from system properties.
+		String osName = System.getProperty("os.name");
+		String arch = System.getProperty("os.arch");
 
-        GrFingerJava.setNativeLibrariesDirectory(destDir);
-        GrFingerJava.setLicenseDirectory(destDir);
-        installed = true;
-    }
+		// Asserts this is a x86 compatible machine.
+		// Fingerprint SDK is currently supported on this CPU architecture.
+		if ((arch.equals("x86")) || (arch.equals("i386"))
+				|| (arch.equals("i486")) || (arch.equals("i586"))
+				|| (arch.equals("i686"))) {
 
-    /**
-     * Finds out which OS / Plataform this JVM is being runned on.
-     */
-    private static int getOS() {
-        //Loads the OS name / CPu architecture from system properties.
-        String osName = System.getProperty("os.name");
-        String arch = System.getProperty("os.arch");
+			// Selects one of the two different supported OS: Win/Linux
+			if (osName.startsWith("Windows"))
+				return WINDOWS_OS;
+			if (osName.startsWith("Linux"))
+				return LINUX_OS;
+		}
 
-        //Asserts this is a x86 compatible machine.
-        //Fingerprint SDK is currently supported on this CPU architecture.
-        if (  (arch.equals("x86")) || (arch.equals("i386")) ||
-              (arch.equals("i486")) || (arch.equals("i586")) ||
-              (arch.equals("i686"))) {
-
-            //Selects one of the two different supported OS: Win/Linux
-            if (osName.startsWith("Windows"))
-                return WINDOWS_OS;
-            if (osName.startsWith("Linux"))
-                return LINUX_OS;
-        }
-
-        //OS has not been recognized (not supported)
-        return UNKNOWN_OS;
-    }
+		// OS has not been recognized (not supported)
+		return UNKNOWN_OS;
+	}
 
 }
